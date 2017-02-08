9ee6fd5d11f111f3b4ed7ac286c728bb1dee0145
tuskerette <vgraffeo@everquote.com>
2017-02-07 20:41:06 -0500

    Can free draw over rectangles.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-(function() {
-  var $ = function(id){return document.getElementById(id)};
+var $ = function(id){return document.getElementById(id)};
+var canvas = window._canvas = new fabric.Canvas('c'),
+  clearEl = $('clear-canvas');
 
-  var canvas = this.__canvas = new fabric.Canvas('c', {
-    isDrawingMode: true
-  });
-  fabric.Object.prototype.transparentCorners = false;
-  var clearEl = $('clear-canvas');
+clearEl.onclick = function() { canvas.clear() };
+canvas.selection = true;
 
-  clearEl.onclick = function() { canvas.clear() };
-
-})();
+canvas.isDrawingMode = true;
+canvas.freeDrawingBrush = new fabric['PencilBrush'](canvas);
+canvas.freeDrawingBrush.color = 'Midnightblue';
+canvas.freeDrawingBrush.width = 2;
+canvas.add(
+  new fabric.Rect({ top: 100, left: 100, width: 50, height: 50, fill: '#ff5555',
+  lockMovementX:true, lockMovementY: true, lockScaling: true, LockRotation:
+true, selectable: false, isDrawingMode: true }),
+  new fabric.Rect({ top: 150, left: 100, width: 50, height: 50, fill: '#ffcc00',
+  lockMovementX:true, lockMovementY: true, lockScaling: true, LockRotation:
+  true, selectable: false, isDrawingMode: true })
+);
+console.log(canvas.freeDrawingBrush);
